0b97106d73c3f7b9be49b1c9ff8760b41e29a1b9
Rafael <stephany@Rafaels-MacBook-Air.local>
2020-12-03 21:34:29 -0500

    updated database relationships

--- Database_setup.py
+++ Database_setup.py
@@ -7,7 +7,8 @@ CREATE TABLE "Wine" (
     "Name" VARCHAR(255)   NOT NULL,
     "Price" Integer   NOT NULL,
     "Description" VARCHAR(255)   NOT NULL,
-    "Merchant" VARCHAR(255)   NOT NULL,
+    "Varietal" VARCHAR(255)   NOT NULL,
+    "Region" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_Wine" PRIMARY KEY (
         "Name"
      )
@@ -18,18 +19,44 @@ CREATE TABLE "Country" (
     "Region" VARCHAR(255)   NOT NULL,
     "State" VARCHAR(255)   NOT NULL,
     "Address" VVARCHAR(255)   NOT NULL,
-    "Name" VARCHAR(255)   NOT NULL
+    CONSTRAINT "pk_Country" PRIMARY KEY (
+        "Country"
+     )
 );
 
 CREATE TABLE "Style" (
-    "Name" VARCHAR(255)   NOT NULL,
     "Sub_type" VARCHAR(255)   NOT NULL,
-    "Varietal" VARCHAR(255)   NOT NULL
+    "Varietal" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Style" PRIMARY KEY (
+        "Sub_type"
+     )
 );
 
-ALTER TABLE "Country" ADD CONSTRAINT "fk_Country_Name" FOREIGN KEY("Name")
+CREATE TABLE "Merchant" (
+    "Merchant" VARCHAR(255)   NOT NULL,
+    "Address" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Merchant" PRIMARY KEY (
+        "Merchant"
+     )
+);
+
+CREATE TABLE "Merchant_wine" (
+    "Wine_name" VARCHAR(255)   NOT NULL,
+    "Merchant" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Merchant_wine" PRIMARY KEY (
+        "Wine_name","Merchant"
+     )
+);
+
+ALTER TABLE "Wine" ADD CONSTRAINT "fk_Wine_Varietal" FOREIGN KEY("Varietal")
+REFERENCES "Style" ("Varietal");
+
+ALTER TABLE "Wine" ADD CONSTRAINT "fk_Wine_Region" FOREIGN KEY("Region")
+REFERENCES "Country" ("Country");
+
+ALTER TABLE "Merchant_wine" ADD CONSTRAINT "fk_Merchant_wine_Wine_name" FOREIGN KEY("Wine_name")
 REFERENCES "Wine" ("Name");
 
-ALTER TABLE "Style" ADD CONSTRAINT "fk_Style_Name" FOREIGN KEY("Name")
-REFERENCES "Wine" ("Name");
+ALTER TABLE "Merchant_wine" ADD CONSTRAINT "fk_Merchant_wine_Merchant" FOREIGN KEY("Merchant")
+REFERENCES "Merchant" ("Merchant");
 
