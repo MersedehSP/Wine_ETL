c49214eca04e715d3ce158694432792f0a73a6cb
Rafael <stephany@Rafaels-Air.fios-router.home>
2020-12-06 14:58:52 -0500

    additional queries

--- more_queries.py
+++ more_queries.py
@@ -9,3 +9,22 @@ GROUP BY country
 ORDER BY "Country Count" DESC LIMIT 10;
 
 SELECT * FROM "Top 10 Wine Producing Countries";
+
+--AVG Varietal Price/Frequency
+CREATE VIEW "Varietal Price/Frequency" AS
+SELECT ROUND(AVG(price),2) AS "AVG Price for a bottle",
+varietal, COUNT(varietal) AS "Varietal Count"
+FROM wine
+GROUP BY varietal
+;
+SELECT * FROM "Varietal Price/Frequency";
+
+--5 AVG Most Expansive bottle throughout all stores
+CREATE VIEW "Most Expansive Bottle" AS
+SELECT wine_name, ROUND(AVG(price),2) AS "AVG Wine Price"
+FROM wine
+GROUP BY wine_name
+ORDER BY "AVG Wine Price" DESC
+LIMIT 5;
+
+SELECT * FROM "Most Expansive Bottle";
